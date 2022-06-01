a41bbdc73945ae2eab0526d4181a7932460ef7a0
André Gomes <88164884+alsgomes21@users.noreply.github.com>
2022-06-01 09:28:52 -0300

    Add files via upload

---
+++ MinhaPrimeiraAtividadePraticaJava.java
@@ -0,0 +1,20 @@
+class MinhaPrimeiraAtividadePraticaJava
+
+{ 
+	//modulo principal com entrada pela linha de comando
+	public static void main (String entrada[])
+	{
+		// declaraçao de variáveis
+		int idade = 33;
+		char genero = 'M';
+		double peso = 70;
+		String nome = "André Gomes";
+		String curso = "Sistema da Informação";
+		
+	
+		System.out.println("Eu sou o " + nome + " tenho " + idade + " anos e tenho " + peso + "kg e curso " + curso);
+                                
+		System.exit(0);
+		
+	}
+}
